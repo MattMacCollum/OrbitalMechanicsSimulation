23c05aed03cb4b9be06bb8a96a325b42121be8a0
Cuji <mmccollum08@qub.ac.uk>
2025-01-16 19:45:33 +0000

    Created solarsystem template and added body class

--- SolarSystemTemplate.py
+++ SolarSystemTemplate.py
@@ -1,8 +1,55 @@
 
+import matplotlib.pyplot as plt
+from vectors import Vector
+
+
 class Solar_System:
     
     def __init__(self, size):
         
         self.size = size
-        bodies = []
+        self.bodies = []
+        
+        
+    def initialise_figure(self):
+        
+        self.fig, self.ax = plt.subplots(
+            1,
+            1,
+            subplot_kw={"projection": "3d"},
+            figsize=(self.size / 50, self.size / 50),
+        )
+        self.fig.tight_layout()
+        
+        
+    
+    def add_body(self, body):
+        self.bodies.append(body)
+        
+        
+        
+        
+
+class SolarSystemBody:
+    
+    def __init__(self, 
+                  solarsystem, 
+                  mass, 
+                  position=(0,0,0), 
+                  velocity=(0,0,0)):
+        self.solarsystem = solarsystem
+        self.mass = mass
+        self.position = position
+        self.velocity = Vector(*velocity)
+        
+        self.solarsystem.add_body(self)
+        
+        
+    
+    def move_body():
+        self.position = (self.position[0] + self.velocity[0],
+                         self.position[0] + self.velocity[0],
+                         self.position[0] + self.velocity[0])
+        
+        
         
